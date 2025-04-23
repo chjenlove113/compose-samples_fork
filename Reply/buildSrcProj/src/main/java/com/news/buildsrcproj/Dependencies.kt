084daef39b18dd64981a1e6e6a22341b2c0065df
chjenlove113 <trajtjmluonnhoveem@gmail.com>
2025-04-23 22:37:35 +0700

    init

---
+++ Reply/buildSrcProj/src/main/java/com/news/buildsrcproj/Dependencies.kt
@@ -0,0 +1,9 @@
+package com.news.buildsrcproj
+
+internal object Dependencies {
+
+    // room-db
+    const val roomCompiler = "androidx.room:room-compiler:${Versions.room}"
+    const val room = "androidx.room:room-ktx:${Versions.room}"
+
+}
